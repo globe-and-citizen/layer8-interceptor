7cf177aacf49aad3ab8c029a92e153d0db8d8fe8
huzaifamk <huzaifamk@pm.me>
2024-06-05 18:20:35 +0500

    Did changes as per Daniel's review

--- interceptor.go
+++ interceptor.go
@@ -464,14 +464,14 @@ func fetch(this js.Value, args []js.Value) interface{} {
 				if body.String() == "<undefined>" {
 					// body = js.ValueOf(map[string]interface{}{}) <= this will err out as "Uncaught (in promise) Error: invalid character '<' looking for beginning of value"
 					body = js.ValueOf("{}")
-					bodyMap["url_path"] = urlPath
+					bodyMap["__url_path"] = urlPath
 				} else {
 					err := json.Unmarshal([]byte(body.String()), &bodyMap)
 					if err != nil {
 						reject.Invoke(js.Global().Get("Error").New(err.Error()))
 						return
 					}
-					bodyMap["url_path"] = urlPath
+					bodyMap["__url_path"] = urlPath
 				}
 				// fmt.Println("[Interceptor] BodyMap: ", bodyMap) // For debugging purposes
 				// encode the body to json
